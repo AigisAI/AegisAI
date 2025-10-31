7aa3ed475ef9ebebaa8de013c6df73ecf6918d08
goodtu02 <qwe13117@gmail.com>
2025-10-31 23:33:02 +0900

    Initial commit

---
+++ src/test/java/org/aegisai/AegisAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aegisai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
