af35af0042ed9326800d0cfdcc016bf765380833
goodtu02 <qwe13117@gmail.com>
2025-11-05 12:14:31 +0900

    Fix. MainController

--- src/main/java/org/aegisai/controller/MainController.java
+++ src/main/java/org/aegisai/controller/MainController.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
     public String main(){
-    return "/index";
+    return "forward:/index";
     }
 
 }
