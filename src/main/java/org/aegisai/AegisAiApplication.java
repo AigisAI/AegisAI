337432fb16fbf8ef2d8560d964d411d5f1060e07
treedaey <a01087504985@gamil.com>
2025-11-06 17:38:09 +0900

    fix. AegisAiApplication.javal

--- src/main/java/org/aegisai/AegisAiApplication.java
+++ src/main/java/org/aegisai/AegisAiApplication.java
@@ -2,17 +2,14 @@ package org.aegisai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class
-})
+@SpringBootApplication  // exclude 제거
+@EnableJpaRepositories("org.aegisai.repository")  // Repository 스캔
+@EntityScan("org.aegisai.entity")                  // Entity 스캔
 public class AegisAiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AegisAiApplication.class, args);
     }
-
 }
